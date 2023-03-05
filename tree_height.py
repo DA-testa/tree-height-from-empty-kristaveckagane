422314e0c712facd931178387b494e3c8b71c602
kristaveckagane <124573561+kristaveckagane@users.noreply.github.com>
2023-03-05 19:20:23 +0000

    test

--- tree_height.py
+++ tree_height.py
@@ -16,12 +16,12 @@ def compute_height(n, parents):
     for i in range(n):
         if mas1[i]==0:
             sec = [(i, 0)]
-        while sec:
-            mezgls, garums = sec.pop(0)
-            mas1[mezgls]=1
-            mas2[mezgls]=garums
-            max_height = max(max_height, garums)
-            for berns in koks[mezgls]:
+            while sec:
+             mezgls, garums = sec.pop(0)
+             mas1[mezgls]=1
+             mas2[mezgls]=garums
+             max_height = max(max_height, garums)
+             for berns in koks[mezgls]:
                 if mas1[berns]==0:
                   sec.append((berns, garums + 1))
     return max_height + 1
@@ -42,18 +42,18 @@ def main():
             print("Nepareiza faila nosaukums. Faila nosaukumā nedrīkst būt burts 'a'.")
             return
         try:
-         with open(file, "r") as f:
-            n = int(f.readline())
-            vecaki = f.readline().strip()
-            parents = numpy.array(list(map(int,vecaki.split())))
-            result=compute_height(n, parents)
+            with open(file, "r") as f:
+              n = int(f.readline())
+              vecaki = f.readline().strip()
+              parents = numpy.array(list(map(int,vecaki.split())))
+              result=compute_height(n, parents)
+              print(result)
         except FileNotFoundError:
             print("fails nav atrasts")
             return
     else:
         print("nepareizi")
-        return
-                print(result)
+
 
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
@@ -66,9 +66,9 @@ def main():
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
-main()
+if _name_ == "__main__":
+ sys.setrecursionlimit(10**7)  # max depth of recursion
+ threading.stack_size(2**27)   # new thread will get stack of such size
+ threading.Thread(target=main).start()
+ main()
 # print(numpy.array([1,2,3]))
