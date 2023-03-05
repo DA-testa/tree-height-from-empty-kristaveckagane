10e54244fb960b80831c989b9b53647758cf7b3a
kristaveckagane <124573561+kristaveckagane@users.noreply.github.com>
2023-03-05 19:49:46 +0000

    test

--- tree_height.py
+++ tree_height.py
@@ -6,59 +6,56 @@ import numpy
 
 
 def compute_height(n, parents):
-    mas1 = [0] * n
-    mas2 = [0] * n
-    koks = [[] for i in range(n)]
+     koks = [[]for i in range(n)]
     for i, vecaks in enumerate(parents):
         if vecaks != -1:
             koks[vecaks].append(i)
-    max_height = 0
+    max_height=0
     for i in range(n):
-        if mas1[i] == 0:
+        mas1 = [0]* n
+        if mas1[i]==0:
             sec = [(i, 0)]
-            while sec:
-                mezgls, garums = sec.pop(0)
-                mas1[mezgls] = 1
-                mas2[mezgls] = garums
-                max_height = max(max_height, garums)
-                for berns in koks[mezgls]:
-                    if mas1[berns] == 0:
-                        sec.append((berns, garums + 1))
+        while sec:
+            mezgls, garums = sec.pop(0)
+            mas1[mezgls]=1
+            max_height = max(max_height, garums)
+            for berns in koks[mezgls]:
+                if mas1[berns]==0:
+                 sec.append((berns, garums + 1))
+                 mas1[berns]=1
     return max_height + 1
-
 
 def main():
     ievade = input("")
-    if text[0] == "I":
+    if "i" in ievade.lower():
         n = int(input(""))
-        vecaki = input("")
-        parents = np.array(list(map(int, vecaki.split())))
-        height = compute_height(n, parents)
-    elif text[0] == "F":
-        file_name = input("")
-        if "a" in file_name:
-            print("nepareizs faila nosaukums")
+        vecaki = input("").strip().split()
+        parents = [int(i) for i in vecaki]
+        result = compute_height(n, parents)
+        print(result)
+    elif "f" in ievade.lower():
+        file = input("")
+        if "a" in file.lower():
+            print("Nepareiza faila nosaukums. Faila nosaukumā nedrīkst būt burts 'a'.")
             return
         try:
-            with open("folder/" + file_name, 'r') as file:
-                n = int(file.readline())
-                vecaki = file.readline().strip()
-                parents = np.array(list(map(int, vecaki.split())))
+            with open(file, "r") as f:
+                n = int(f.readline())
+                vecaki = f.readline().strip().split()
+                parents = [int(i) for i in vecaki]
                 result = compute_height(n, parents)
+                print(result)
         except FileNotFoundError:
-            print("Fails nav atrasts")
+            print("fails nav atrasts")
             return
     else:
-        print("Nepareiza ievade")
-        return
-    print(result)
+        print("nepareizi")
 
-if _name_ == '_main_':
-    sys.setrecursionlimit(10**7)
-    threading.stack_size(2**27)
-    thread = threading.Thread(target=main)
-    thread.start()
+
+if __name__ == "__main__":
+    sys.setrecursionlimit(10**7)  # max depth of recursion
+    threading.stack_size(2**27)   # new thread will get stack of such size
+    threading.Thread(target=main).start()
     main()
-
 
 # print(numpy.array([1,2,3]))
