94f9e3f270c916409efb7282ee7d41676cf05861
kristaveckagane <124573561+kristaveckagane@users.noreply.github.com>
2023-03-05 17:23:55 +0000

    test

--- tree_height.py
+++ tree_height.py
@@ -56,7 +56,7 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    pass
+
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
